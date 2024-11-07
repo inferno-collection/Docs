e2387a8527bd1675a452e0f00095c1e4b0f87f1b
Christopher M. <mrcm8263@gmail.com>
2024-11-07 12:31:04 +1100

    Update colors and footer links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -5,111 +5,123 @@ import type * as Preset from '@docusaurus/preset-classic';
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'Inferno Collection',
-  tagline: 'Where all the Fire/EMS magic happens',
-  favicon: 'img/favicon.ico',
+	title: 'Inferno Collection',
+	tagline: 'Where all the Fire/EMS magic happens',
+	favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://docs.inferno-collection.com/',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+	// Set the production url of your site here
+	url: 'https://docs.inferno-collection.com/',
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: '/',
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'inferno-collection', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: '', // Usually your GitHub org/user name.
+	projectName: '', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: 'en',
+		locales: ['en'],
+	},
 
-  presets: [
-    [
-      'classic',
-      {
-        docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          // editUrl: '',
-        },
-        blog: false,
-        theme: {
-          customCss: './src/css/custom.css',
-        },
-      } satisfies Preset.Options,
-    ],
-  ],
+	presets: [
+		[
+			'classic',
+			{
+				docs: {
+					sidebarPath: './sidebars.ts',
+					routeBasePath: '/',
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					// editUrl: '',
+				},
+				blog: false,
+				theme: {
+					customCss: './src/css/custom.css',
+				},
+			} satisfies Preset.Options,
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
-    navbar: {
-      title: 'Inferno Collection',
-      logo: {
-        alt: 'Inferno Collection',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {
-          type: 'docSidebar',
-          sidebarId: 'docsSidebar',
-          position: 'left',
-          label: 'Docs',
-        },
-      ],
-    },
-    footer: {
-      style: 'dark',
-      links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Fire Alarm Reborn',
-              to: '/fire-alarm-reborn',
-            },
-          ],
-        },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Discord',
-              href: 'https://discord.gg/5GZ3Jzt',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/inferno-collection',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
-        },
-      ],
-      copyright: `Copyright © ${new Date().getFullYear()} Inferno Collection. Built with Docusaurus.`,
-    },
-    prism: {
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+	themeConfig: {
+		// Replace with your project's social card
+		image: 'img/docusaurus-social-card.jpg',
+		colorMode: {
+			defaultMode: 'dark',
+			disableSwitch: true,
+		},
+		navbar: {
+			title: 'Inferno Collection',
+			logo: {
+				alt: 'Inferno Collection',
+				src: 'img/logo.svg',
+			},
+			items: [
+				{
+					type: 'docSidebar',
+					sidebarId: 'docsSidebar',
+					position: 'left',
+					label: 'Docs',
+				},
+			],
+		},
+		footer: {
+			style: 'dark',
+			links: [
+				{
+					title: 'Docs',
+					items: [
+						{
+							label: 'Fire Alarm Reborn',
+							to: '/fire-alarm-reborn',
+						},
+					],
+				},
+				{
+					title: 'Community',
+					items: [
+						{
+							label: 'Website',
+							href: 'https://inferno-collection.com/',
+						},
+						{
+							label: 'Discord',
+							href: 'https://inferno.gay/discord',
+						},
+						{
+							label: 'YouTube',
+							href: 'https://inferno.gay/yt',
+						},
+					],
+				},
+				{
+					title: 'More',
+					items: [
+						{
+							label: 'Store',
+							href: 'https://store.inferno-collection.com/',
+						},
+						{
+							label: 'GitHub',
+							href: 'https://github.com/inferno-collection/',
+						},
+					],
+				},
+			],
+			copyright: `Copyright © 2019-${new Date().getFullYear()} Inferno Collection.<br>Built with Docusaurus.`,
+		},
+		prism: {
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
