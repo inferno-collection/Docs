b5383781bc1c1efd114dbc0ba31094c3bec53e27
Christopher M. <mrcm8263@gmail.com>
2026-09-21 16:43:02 +1000

    Adds remaining links

--- src/link-previews/resource-overrides.ts
+++ src/link-previews/resource-overrides.ts
@@ -54,26 +54,188 @@ const RESOURCE_PREVIEW_OVERRIDES: Record<string, DocsPreviewDefinition> = {
   'hands-free-siren': {
     imagePath: '/img/links/hfs.png',
     imageAlt: 'Hands-Free Siren',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/hands-free-siren/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/hands-free-siren/config',
+      },
+      {
+        label: 'Usage',
+        url: 'https://docs.inferno-collection.com/resources/hands-free-siren/usage',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/hands-free-siren/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
   flares: {
     imagePath: '/img/links/iflares.png',
-    imageAlt: 'I-Flares',
+    imageAlt: 'IFlares',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/flares/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/flares/config',
+      },
+      {
+        label: 'Devs',
+        url: 'https://docs.inferno-collection.com/resources/flares/developers/',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/flares/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
   'pager-reborn': {
     imagePath: '/img/links/pr.png',
     imageAlt: 'Pager Reborn',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/pager-reborn/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/pager-reborn/config',
+      },
+      {
+        label: 'Devs',
+        url: 'https://docs.inferno-collection.com/resources/pager-reborn/developers/',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/pager-reborn/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
   'station-alert': {
     imagePath: '/img/links/sa.png',
     imageAlt: 'Station Alert',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/station-alert/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/station-alert/config',
+      },
+      {
+        label: 'Devs',
+        url: 'https://docs.inferno-collection.com/resources/station-alert/developers/',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/station-alert/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
   spotlight: {
     imagePath: '/img/links/spotlight.png',
     imageAlt: 'Spotlight',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/spotlight/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/spotlight/config',
+      },
+      {
+        label: 'Devs',
+        url: 'https://docs.inferno-collection.com/resources/spotlight/developers/',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/spotlight/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
   torches: {
     imagePath: '/img/links/torches.png',
     imageAlt: 'Torches',
+    buttons: [
+      {
+        label: 'Install',
+        url: 'https://docs.inferno-collection.com/resources/torches/install',
+      },
+      {
+        label: 'Config',
+        url: 'https://docs.inferno-collection.com/resources/torches/config',
+      },
+      {
+        label: 'Devs',
+        url: 'https://docs.inferno-collection.com/resources/torches/developers/',
+      },
+      {
+        label: 'Changelog',
+        url: 'https://docs.inferno-collection.com/resources/torches/changelog',
+      },
+      {
+        label: 'Discord',
+        url: 'https://discord.com/invite/5GZ3Jzt',
+        emoji: {
+          name: 'discord',
+          id: '1551442516341956638',
+          animated: true,
+        },
+      },
+    ],
   },
 };
 
